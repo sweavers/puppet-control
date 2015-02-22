9a900f8e89a46f2e55c3c5084c51946248f174b6
Ben Fairless <ben.fairless@landregistry.gsi.gov.uk>
2015-02-22 03:20:41 +0000

    Expanded MongoDB features

--- site/profiles/manifests/mongodb.pp
+++ site/profiles/manifests/mongodb.pp
@@ -1,44 +1,90 @@
-# Class profiles::puppet
+# Class profiles::mongodb
 #
-# This class will manage Puppet Agent and Master installations
+# This class will manage MongoDB installations
 #
 # Parameters:
-#  ['master'] - Boolean.
+#  ['port']     - Port which MongoDB should listen on. Defaults = 27018
+#  ['version']  - Version of MongoDB to install. Default = 2.6.7
+#  ['remote']   - Should MongoDB listen for remote connections. Defaults true
+#  ['password'] - Hex encoded MD5 of $username:mongo:$password for admin user
+#  ['dbroot']   - Location installation should be placed. Defaults = /mongodb
+#  ['dbpath']   - Location of database. Defaults to $dbroot/data
+#  ['logpath']  - Location of log directory. Default = $dbroot/log
 #
 # Requires:
-# - Hello
+# - puppetlabs/mongodb
+# - puppetlabs/stdlib
 #
 # Sample Usage:
-#   class { 'profiles::puppet': }
+#   class { 'profiles::mongodb':
+#     remote => ['192.168.1.2']
+#   }
 #
 class profiles::mongodb(
 
-  $port    = 27018,
-  $dbroot  = '/mongodb/',
-  $dbpath  = 'data/',
-  $config  = 'config/',
-  $logpath = 'log/',
+  $port     = 27018,
+  $version  = '2.6.7',
+  $remote   = true,
+  $password = '81cfce99f33d674bdd240d8c9d8ae44d', # 'mongodb'
+  $dbroot   = '/mongodb',
+  $dbpath   = 'data/',
 
 ){
 
-  file { $dbroot:
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0777',
+
+  # Set bind address to 0.0.0.0 if remote is enabled, 127.0.0.1 if not
+  # Merge remote into an address array if it's anything other than a boolean
+  if $remote == true {
+    $bind = '0.0.0.0'
+  } elsif $remote == false {
+    $bind = '127.0.0.1'
+  } else {
+    $bind_array = delete(any2array($remote),'127.0.0.1')
+    $bind = concat(['127.0.0.1'],$bind_array)
   }
 
-  class {'mongodb::server':
+  # Use 10gen repositories instead of distribution's
+  class { 'mongodb::globals':
+    manage_package_repo => true,
+    version             => $version
+  }->
+
+  class { 'mongodb::client':
+    require => Class['Mongodb::globals']
+  }
+
+  class { 'mongodb::server':
     ensure  => present,
     port    => $port,
+    bind_ip => $bind, # ? error
     verbose => true,
     auth    => true,
     dbpath  => "${dbroot}/${dbpath}",
-    config  => "${dbroot}/${config}",
     journal => true,
-    logpath => "${dbroot}/${logpath}/${::hostname}.log",
-    rest    => true,
-    require => File[$dbroot]
+    rest    => false,
+    require => [Class['Mongodb::client'],File[$dbroot]]
+  }
+
+  user { 'mongodb':
+    ensure     => present,
+    comment    => 'MongoDB Database Server',
+    system     => true,
+    home       => $dbroot,
+    managehome => true,
+    shell      => '/usr/sbin/nologin',
+  }
+
+  file { $dbroot:
+    ensure  => 'directory',
+    owner   => 'mongodb',
+    group   => 'mongodb',
+    mode    => '0750',
+    require => User[mongodb]
+  }
+
+  package { ['mongodb-org-tools']:
+    ensure => present,
+    before => Class['mongodb::server']
   }
 
 }
