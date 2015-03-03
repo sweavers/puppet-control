c9db9cdc19545c1e26b55d7b78d3cc7916080217
stevechambers <stevechambers@users.noreply.github.com>
2015-03-03 17:32:35 +0000

    Amend package list

--- site/profiles/manifests/appserver.pp
+++ site/profiles/manifests/appserver.pp
@@ -38,19 +38,15 @@ class profiles::appserver(
   case $::osfamily{
     'RedHat': {
       $PKGLIST=['java-1.7.0-openjdk','java-1.7.0-openjdk-devel','python',
-                'python-devel','ruby','rubygems','bison','byacc','cscope',
-                'ctags','cvs','diffstat','doxygen','flex','gcc','gcc-c++',
-                'gcc-gfortran','gettext','git','indent','intltool','libtool',
-                'patch','patchutils','rcs','redhat-rpm-config','rpm-build',
-                'subversion','swig','systemtap']
+                'python-devel','ruby','rubygems','autoconf','automake',
+                'binutils','bison','flex','gcc','gcc-c++','gettext','libtool',
+                'make','patch','pkgconfig','redhat-rpm-config','rpm-build',
+                'rpm-sign']
       $PYTHON='lr-python3-3.4.3-1.x86_64.rpm'
       $PKGMAN='rpm'
     }
     'Debian': {
-      $PKGLIST=['openjdk-7-jdk','python','python-dev','ruby','byacc','cscope',
-                'exuberant-ctags','cvs','diffstat','doxygen','gcc','g++',
-                'gfortran','gettext','git','indent','intltool','libtool',
-                'patch','patchutils','rcs','subversion','swig','systemtap']
+      $PKGLIST=['openjdk-7-jdk','python','python-dev','ruby']
       $PYTHON='lr-python3_3.4.3_amd64.deb'
       $PKGMAN='dpkg'
     }
