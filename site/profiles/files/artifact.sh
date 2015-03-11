f64909ff2ecb64100fee483ab334b3146e2f9bc0
stevechambers <stevechambers@users.noreply.github.com>
2015-03-11 11:24:04 +0000

    Remove original script

--- site/profiles/files/artifact.sh
+++ site/profiles/files/artifact.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-# Builds a tar ball of the /etc/puppet/environments folder for deployment to puppet masters
+# Builds a tar ball of the /etc/puppet/environments folder (including secrets) for deployment to puppet masters
 
 GITREPO=$1
+SECRETS=$2
 DIR="/etc/artifact_r10k"
 TIMESTAMP=$(date +%d%m%Y-%H%M)
+USAGE=`basename $0`" [GITREPO] [SECRETS_REPO]"
+TEMPDIR="/tmp/secrets-$(date +%s)"
+HIERA_PATH="hiera/secrets"
+OUTPUT_LABEL='deploy-secrets'
 
 # Purely cosmetic function to prettify output
 # Set OUTPUT_LABEL to change the label
@@ -27,12 +32,15 @@ function output() {
 # Check user is root
 [ $(id -u) != 0 ] && echo "ERROR - You must run this script as root!" | output ERROR && exit 1
 
-# Check that a repo has been specified
-[ -z "$GITREPO" ] && echo "ERROR - You must specify a source git repo!" | output ERROR && exit 1
+# Check that a git repo has been specified
+[ -z "$GITREPO" ] && echo -e "ERROR - You must specify a source git repo! \n Usage: ${USAGE}" | output ERROR && exit 1
+
+# Check that a secrets repo has been specified
+[ -z "$SECRETS" ] && echo -e "ERROR - You must specify a secrets repo! \n Usage: ${USAGE}" | output ERROR && exit 1
 
 # Ensure that /usr/local/bin/ is on root's path
 if ! printenv | grep PATH | grep /usr/local/bin/; then
-  export PATH=$PATH:/usr/local/bin && echo "Adding /usr/local/bin to PATH" | output SUCCESS
+  export PATH=$PATH:/usr/local/bin && echo "Adding /usr/local/bin to PATH" | output
 fi
 
 # Check that required commands are installed
@@ -62,7 +70,7 @@ else
 fi
 
 # Run R10k to get puppet code from github and dependency modules from the puppet forge
-echo "Attempting to retrive puppet environment code and dependencies" | output SUCCESS
+echo "Attempting to retrive puppet environment code and dependencies" | output
 r10k deploy environment -c /etc/artifact_r10k.yaml > /dev/null 2>&1
 if [[ $? == '0' ]]; then
   echo "Puppet environment code and dependencies successfully retrieved" | output SUCCESS
@@ -70,14 +78,49 @@ else
   echo "Error retriving puppet environment code and or dependencies" | output ERROR && exit 1
 fi
 
+# Use git to pull secrests from secrets repo
+echo "Attempting to retrive secrets" | output
+git clone ${SECRETS} ${TEMPDIR} 2>&1 | output
+
+for environment in ${DIR}/environments/*; do
+  envname=$(basename ${environment})
+
+  # Clean up old secrets directory or create new one
+  if [ -d "${environment}/${HIERA_PATH}" ]; then
+    echo "Purging previous secrets from ${envname}" | output
+    rm -rf ${environment}/${HIERA_PATH}/*
+  else
+    mkdir ${environment}/${HIERA_PATH}
+  fi
+
+  # Check to see if branch exists in secrets repository that matches environment
+  git -c ${TEMPDIR} checkout ${envname} >/dev/null 2>&1
+  if [[ $? == '0' ]]; then
+    # Deploy new secrets
+    cp -R ${TEMPDIR}/* ${environment}/${HIERA_PATH}
+    if [ $? == '0' ]; then
+      echo "New secrets deployed to ${envname}" | output
+    else
+      echo "ERROR - Unable to deploy secrets from ${TEMPDIR} to ${environment}" | output ERROR
+      SOFTFAIL+=("${envname}")
+      break # Skip to next environment
+    fi
+  else
+    echo "No secrets available for ${envname}" | output WARN
+  fi
+
+done
+
+# Purge temp secrets directory
+rm -rf ${TEMPDIR} 2>&1 >/dev/null
+
 # Enssure that target artifact directory exits
 if [ ! -d ${DIR}/artifacts ] ; then
   mkdir -p ${DIR}/artifacts
 fi
 
 # Create compressed tar archive of the puppet enviroment code and dependancies
-echo "Attempting to create artifact" | output SUCCESS
-
+echo "Attempting to create artifact" | output
 tar -C ${DIR} -czf ${DIR}/artifacts/${TIMESTAMP}.tgz environments/ > /dev/null 2>&1
 if [[ $? == '0' ]]; then
   echo "Artifact successfully created" | output SUCCESS && exit 0
