6efae026afac416b59c7c0ab39bfb9d9f20829c8
stevechambers <stevechambers@users.noreply.github.com>
2015-03-12 14:42:03 +0000

    Add deploy.sh
    Script to deploy artifact archive to puppet masters and remote ci servers

---
+++ site/profiles/files/deploy.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+# Deploy tar ball of the /etc/puppet/environments folder (including secrets) for deployment to puppet masters
+
+DIR="/var/lib/jenkins/artifact_r10k"
+#PUPPETMASTER="puppet.lnx.lr.net"
+PUPPETMASTER="192.168.39.5 192.168.39.5"
+REMOTECI="192.168.39.5 192.168.39.5"
+ARTIFACT=`ls ${DIR}/artifacts/ | sort -n | tail -1`
+
+# Purely cosmetic function to prettify output
+# Set OUTPUT_LABEL to change the label
+# Supports ERROR, SUCCESS, and WARN as arguments
+function output() {
+local label=${OUTPUT_LABEL:-$0}
+local timestamp=$(date +%d/%m/%Y\ %H:%M)
+local colour='\033[34m' # Blue
+local reset='\033[0m'
+case $1 in
+ERROR) local colour='\033[31m' ;; # Red
+SUCCESS) local colour='\033[32m' ;; # Green
+WARN) local colour='\033[33m' ;; # Yellow
+esac
+while read line; do
+echo -e "${colour}${label} [${timestamp}]${reset} ${line}"
+done
+}
+
+# Check prerequisits
+# Check user is root - currently disabled as script is now configured to run as un unpriv user
+#[ $(id -u) != 0 ] && echo "ERROR - You must run this script as root!" | output ERROR && exit 1
+
+for PUPPET in ${PUPPETMASTER} ; do
+  
+  # Copy artifact to puppet master
+  echo "Attempting to copy ${ARTIFACT} to ${PUPPET}" | output
+  scp ${DIR}/artifacts/${ARTIFACT} deployment@${PUPPET}:/tmp/ > /dev/null 2>&1
+  if [[ $? == '0' ]]; then
+    echo "${ARTIFACT}" successfully copied to ${PUPPET} | output SUCCESS
+  else
+    echo "Error copying ${ARTIFACT} to ${PUPPET}" | output ERROR && exit 1
+  fi
+  
+  # SSH to puppet master, stop puppetmaster service, purge existing environments,
+  # uncompress artifact and restart puppetmaster service 
+  ssh deployment@${PUPPET} "sudo service apache2 stop" > /dev/null 2>&1
+  [[ $? != '0' ]] && echo "Error stopping apache2  service on ${PUPPET}" | output ERROR && exit 1   
+  ssh deployment@${PUPPET} "sudo rm -rf /etc/puppet/environments" > /dev/null 2>&1
+  [[ $? != '0' ]] && echo "Error purging existing environments on  ${PUPPET}" | output ERROR && exit 1
+  ssh deployment@${PUPPET} "sudo tar -C /etc/puppet/ -xzf /tmp/${ARTIFACT}" > /dev/null 2>&1 
+  [[ $? != '0' ]] && echo "Error extracting artifact on  ${PUPPET}" | output ERROR && exit 1
+  ssh deployment@${PUPPET} "sudo service apache2 start" > /dev/null 2>&1
+  if [[ $? == '0' ]]; then
+    echo "Puppet environment code and dependencies successfully deployed" | output SUCCESS
+  else
+    echo "Error restarting apache2 service on ${PUPPET}" | output ERROR && exit 1
+  fi
+done
+
+# Copy artifact to remote ci server
+for CI in ${REMOTECI} ; do
+  echo "Attempting to copy ${ARTIFACT} to ${CI}" | output
+  scp ${DIR}/artifacts/${ARTIFACT} deployment@${CI}:/tmp/ > /dev/null 2>&1
+  if [[ $? == '0' ]]; then
+    echo "${ARTIFACT}" successfully copied to ${CI} | output SUCCESS
+  else
+    echo "Error copying ${ARTIFACT} to ${CI}" | output ERROR && exit 1
+  fi
+done
