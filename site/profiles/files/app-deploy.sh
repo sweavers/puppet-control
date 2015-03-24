5b86acb069296da831a339c136a77aaf028cf250
stevechambers <stevechambers@users.noreply.github.com>
2015-03-24 18:06:09 +0000

    Debugged app-deployment script

--- site/profiles/files/app-deploy.sh
+++ site/profiles/files/app-deploy.sh
@@ -2,9 +2,10 @@
 # Create and deploy a tar ball containing beta application code
 
 GITREPO=${GITREPO:-""}
+APPNAME=`echo $GITREPO | cut -d "/" -f2 | cut -d "." -f1`
 TARGETSERVER=${TARGETSERVER:-""}
-LOCALDIR="/tmp/${GITREPO}"
-TARGETDIR="/var/deployment/${GITREPO}"
+LOCALDIR="/tmp/"
+TARGETDIR="/opt/deployment/${APPNAME}"
 
 # Purely cosmetic function to prettify output
 # Set OUTPUT_LABEL to change the label
@@ -28,73 +29,67 @@ done
 # Check user is root - currently disabled as script is now configured to run as un unpriv user
 #[ $(id -u) != 0 ] && echo "ERROR - You must run this script as root!" | output ERROR && exit 1
 
-# Clean up old LOCALDIR or create new one
-if [ -d "${LOCALDIR}" ]; then
-  echo "Purging old code from ${LOCALDIR}" | output
-  rm -rf ${LOCALDIR}/*
-else
-  echo "Creating ${LOCALDIR}" | output
-  mkdir -p ${LOCALDIR}
-fi
-
 # Clone GITREPO to LOCALDIR
 echo "Attempting to clone ${GITREPO}" | output
-git clone ${GITREPO} ${LOCALDIR}
+git clone ${GITREPO} ${LOCALDIR}${APPNAME} > /dev/null 2>&1
 if [[ $? == '0' ]]; then
-  echo "Successfully cloned ${GITREPO}" | output SUCCESS && exit 0
+  echo "Successfully cloned ${GITREPO}" | output SUCCESS
 else
   echo "Error creating cloning ${GITREPO}" | output ERROR && exit 1
 fi
 
 # Create compressed tar archive of the git repo
 echo "Attempting to create code artifact" | output
-tar -C ${LOCALDIR} -czf ${GITREPO}.tgz ${GITREPO}/ > /dev/null 2>&1
+tar -C ${LOCALDIR}/${APPNAME} -czf ${APPNAME}.tgz . > /dev/null 2>&1
 if [[ $? == '0' ]]; then
-  echo "Code artifact successfully created" | output SUCCESS && exit 0
+  echo "Code artifact successfully created" | output SUCCESS
 else
   echo "Error creating code artifact" | output ERROR && exit 1
 fi
 
 # Copy tar archive to TARGETSERVER
 echo "Attempting to copy code artifact to ${TARGETSERVER}" | output
-scp ${LOCALDIR}/${GITREPO}.tgz deployment@${TARGETSERVER}/tmp
+scp ${APPNAME}.tgz deployment@${TARGETSERVER}:/tmp/ > /dev/null 2>&1
 if [[ $? == '0' ]]; then
-  echo "Successfully copied code artifact to ${TARGETSERVER}" | output SUCCESS && exit 0
+  echo "Successfully copied code artifact to ${TARGETSERVER}" | output SUCCESS
 else
   echo "Error copying code artifact to ${TARGETSERVER}" | output ERROR && exit 1
 fi
 
 # SSH to TARGETSERVER,
-#stop monitd service
-ssh deployment@${TARGETSERVER} "sudo service monitd stop" > /dev/null 2>&1
-[[ $? != '0' ]] && echo "Error stopping monitd service on ${TARGETSEVER}" | output ERROR && exit 1
+echo "Attempting to deploy code artifact on ${TARGETSERVER}" | output
+# Stop monit service
+ssh deployment@${TARGETSERVER} "sudo monit stop all" > /dev/null 2>&1
+[[ $? != '0' ]] && echo "Error stopping monit service on ${TARGETSERVER}" | output ERROR && exit 1
 
 # Ensure 'prev-ver' directory exists and is purged on TARGETSERVER
-ssh deployment@${TARGETSERVER} "sudo if [ ! -d /var/deployment/prev-ver ]; \
-then sudo mkdir -p /var/deployment/prev-ver; \
-else rm -rf /var/deployment/prev-ver" > /dev/null 2>&1
-[[ $? != '0' ]] && echo "Error creating or purging 'prev-ver' directory on ${TARGETSEVER}" | output ERROR && exit 1
+ssh deployment@${TARGETSERVER} "if [ ! -d /opt/deployment/prev-ver ] ; then sudo mkdir -p /opt/deployment/prev-ver ; else sudo rm -rf /opt/deployment/prev-ver/* ; fi" #> /dev/null 2>&1
+[[ $? != '0' ]] && echo "Error creating or purging 'prev-ver' directory on ${TARGETSERVER}" | output ERROR && exit 1
+
+# Ensure TARGETDIR directory exists on TARGETSERVER
+ssh deployment@${TARGETSERVER} "if [ ! -d ${TARGETDIR} ] ; then sudo mkdir -p ${TARGETDIR} ; else exit 0 ; fi" #> /dev/null 2>&1
+[[ $? != '0' ]] && echo "Error creating or creating ${TARGETDIR} on ${TARGETSERVER}" | output ERROR && exit 1
 
 # Move existing code to 'prev-ver' folder
-ssh deployment@${TARGETSERVER} "sudo mv ${TARGETDIR}/* /var/deployment/prev-ver/" > /dev/null 2>&1
-[[ $? != '0' ]] && echo "Error moving existing code to 'pre-ver' ${TARGETSEVER}" | output ERROR && exit 1
+ssh deployment@${TARGETSERVER} "sudo mv ${TARGETDIR}/* /opt/deployment/prev-ver/" > /dev/null 2>&1
+[[ $? != '0' ]] && echo "Error moving existing code to 'pre-ver' ${TARGETSERVER}" | output ERROR && exit 1
 
 # Untar artifact from /tmp to TARGETDIR
-ssh deployment@${TARGETSERVER} "sudo tar -C ${TARGETDIR} -xzf /tmp/${GITREPO}.tgz/" > /dev/null 2>&1
-[[ $? != '0' ]] && echo "Error unpacking ${GITREPO}.tgz on ${TARGETSEVER}" | output ERROR && exit 1
+ssh deployment@${TARGETSERVER} "sudo tar -C ${TARGETDIR} -xzf /tmp/${APPNAME}.tgz" > /dev/null 2>&1
+if [[ $? == '0' ]]; then
+  echo "Code artifact successfully deployed to ${TARGETSERVER}" | output SUCCESS
+else
+  echo "Error unpacking ${APPNAME}.tgz on ${TARGETSREVER}" | output ERROR && exit 1
+fi
 
 # Re-start monitd service
-ssh deployment@${TARGETSERVER} "sudo service monitd start" > /dev/null 2>&1
-[[ $? != '0' ]] && echo "Error re-starting monitd service on ${TARGETSEVER}" | output ERROR && exit 1
+ssh deployment@${TARGETSERVER} "sudo monit start all" > /dev/null 2>&1
+[[ $? != '0' ]] && echo "Error re-starting monitd service on ${TARGETSERVER}" | output ERROR && exit 1
 
 # Clean up
-ssh deployment@${TARGETSERVER} "sudo rm -rf /tmp/${GITREPO}.tgz/" > /dev/null 2>&1
-[[ $? != '0' ]] && echo "Error cleaning up /tmp/${GITREPO}.tgz on ${TARGETSEVER}" | output WARN
-
-# Test service
-ssh deployment@${TARGETSERVER} "curl localhost" > /dev/null 2>&1
-if [[ $? == '0' ]]; then
-  echo "Application code successfully deployed to ${TARGETSEVER}" | output SUCCESS
-else
-  echo "Cannot access application service on ${TARGETSEVER}" | output ERROR && exit 1
-fi
+ssh deployment@${TARGETSERVER} "sudo rm -rf /tmp/${APPNAME}.tgz/" > /dev/null 2>&1
+[[ $? != '0' ]] && echo "Error cleaning up /tmp/${APPNAME}.tgz on ${TARGETSERVER}" | output WARN
+rm ${APPNAME}.tgz
+[[ $? != '0' ]] && echo "Error cleaning up /tmp/${APPNAME}.tgz on local machine" | output WARN
+rm -rf ${LOCALDIR}${APPNAME}
+[[ $? != '0' ]] && echo "Error cleaning up ${LOCALDIR}${APPNAME} on local machine" | output WARN
