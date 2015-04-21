d786a32310f9bf52685121cc39ea54a1b3a9d37e
Mikael <mikellallison@gmail.com>
2015-04-21 13:58:02 +0100

    removing config

--- site/profiles/manifests/nginx.pp
+++ site/profiles/manifests/nginx.pp
@@ -15,47 +15,7 @@ class profiles::nginx (
 
   include ::nginx
 
-  if $virtual == 'xenhvm' {
-
-  	case $::hostname {
-  		'digital-register-frontend-01': {
-
-  			nginx::resource::vhost { 'digital.integration.beta.landregistryconcept.co.uk':
-              
-              listen_port     => 80,
-              proxy           => 'http://127.0.0.1:8000',
-              www_root        => '/var/jail',
-              proxy_set_header=> [
-              'X-Real-IP        $remote_addr',
-              'X-Forwarded-For  $proxy_add_x_forwarded_for', # This directive is to address US100 "session stealing"
-              'Host             $http_host',
-              ],
-
-  		    }
-
-  	
-        }
-        'digital-register-frontend-02': {
-
-        	nginx::resource::vhost { 'digital.preview.beta.landregistryconcept.co.uk':
-             
-              listen_port      => 80,
-              proxy            => 'http://127.0.0.1:8000',
-              www_root         => '/var/jail',
-              proxy_set_header => [
-              'X-Real-IP         $remote_addr',
-              'X-Forwarded-For   $proxy_add_x_forwarded_for',
-              'Host              $http_host',
-              ],
-
-            }
-
-        }
-  	}
-  }
-  else {
-  		# enter puppet code
-  }
+ 
   
 
 }
