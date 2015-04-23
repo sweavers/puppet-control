41e3f95e99ac2e96f0bd6557385b3ca79ddc7671
Mikael <mikellallison@gmail.com>
2015-04-23 14:35:47 +0100

    We dont require the deployment profile for vagrant

---
+++ site/profiles/manifests/vagrant.pp
@@ -0,0 +1,75 @@
+# Class profiles::digitalregister_app
+#
+# This class will manage api server installations
+#
+# Requires:
+# - puppetlabs/stdlib
+#
+# Sample Usage:
+#   class { 'profiles::digitalregister_app': }
+#
+class profiles::vagrant{
+
+  include ::stdlib
+  include ::profiles::nginx
+
+  #  Install required packages for Ruby and Java
+  case $::osfamily{
+    'RedHat': {
+      $PKGLIST=['python','python-devel','epel-release']
+      $PYTHON='lr-python3-3.4.3-1.x86_64'
+      $PYPGK="${PYTHON}.rpm"
+      $PKGMAN='rpm'
+    }
+    'Debian': {
+      $PKGLIST=['python','python-dev','python-pip']
+      $PYTHON='lr-python3_3.4.3_amd64'
+      $PYPGK="${PYTHON}.deb"
+      $PKGMAN='dpkg'
+    }
+    default: {
+      fail("Unsupported OS type - ${::osfamily}")
+    }
+  }
+  ensure_packages($PKGLIST)
+
+  package {
+      'python-pip':
+          ensure      => installed,
+          provider    => yum,
+          require     => Package[$PKGLIST];
+  }
+
+  file{'LR Python package':
+    ensure => 'file',
+    path   => "/tmp/${PYPGK}",
+    source => "puppet:///modules/profiles/${PYPGK}"
+  }
+
+  # Install custom Python 3.4.3 build
+  package{ $PYTHON :
+    ensure   => installed,
+    provider => $PKGMAN,
+    source   => "/tmp/${PYPGK}",
+    require  => File['LR Python package']
+  }
+
+  file{'/usr/bin/pip3' :
+    ensure  => link,
+    target  => '/usr/local/bin/pip3',
+    require => Package[$PYTHON]
+  }
+
+  package{'gunicorn' :
+    ensure   => installed,
+    provider => pip3,
+    require  => File['/usr/bin/pip3']
+  }
+
+  package{'flask' :
+    ensure   => installed,
+    provider => pip3,
+    require  => File['/usr/bin/pip3']
+  }
+
+}
