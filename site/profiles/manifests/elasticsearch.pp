b2d25e956d08b0e1e081792808e38db4cc8ee87b
stevechambers <stevechambers@users.noreply.github.com>
2015-04-24 16:30:30 +0100

    Install Java

--- site/profiles/manifests/elasticsearch.pp
+++ site/profiles/manifests/elasticsearch.pp
@@ -9,8 +9,9 @@ class profiles::elasticsearch {
 
   class {'::elasticsearch' :
     manage_repo  => true,
-    repo_version => 1.4
+    repo_version => 1.4,
+    java_install => true
   }
-  
+
   elasticsearch::instance { 'es-01': }
 }
