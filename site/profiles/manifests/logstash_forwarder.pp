d6fd35e123dd7af6a99758d84bc63ca69b28b1c5
Lez Light <lezlightlr@gmail.com>
2015-04-29 11:10:32 +0100

    Update logstash_forwarder.pp

--- site/profiles/manifests/logstash_forwarder.pp
+++ site/profiles/manifests/logstash_forwarder.pp
@@ -1,18 +1,15 @@
-# Class profiles::logstash_forwarder
+# Class profiles::log_forwarder
 #
 # Will install logstash forwarder on a node.
 #
 # Sample Usage:
-#   class { 'profiles::logstash_forwarder': }
+#   class { 'profiles::log_forwarder': }
 #
-class profiles::logstash_forwarder(
-  $environment = undef
-  )
-
-  {
-
-  include profiles::logbroker_extranet
-  include profiles::log_repository
+class profiles::log_forwarder(
+  $environment = undef,
+#  $logserver_ip = undef,
+#  $logserver_cert = undef
+) {
 
   $ip_first_octet = split( $::ipaddress, '[.]' )
 
@@ -20,13 +17,13 @@ class profiles::logstash_forwarder(
     0: {
       case $ip_first_octet[0]{
         10: {
-          $logserver_ip   = $profiles::logbroker_extranet::prod_ip_address
-          $logserver_cert = $profiles::logbroker_extranet::prod_logstash_forwarder_cert
+          $logserver_ip   = hiera('logbroker_extranet_prod_ip_address')
+          $logserver_cert = hiera('logbroker_extranet_prod_logstash_forwarder_cert')
         }
 
         192: {
-          $logserver_ip   = $profiles::log_repository::prod_ip_address
-          $logserver_cert = $profiles::log_repository::prod_logstash_forwarder_cert
+          $logserver_ip   = hiera('log_repository_prod_ip_address')
+          $logserver_cert = hiera('log_repository_prod_logstash_forwarder_cert')
         }
 
         default: {
@@ -37,13 +34,13 @@ class profiles::logstash_forwarder(
     1: {
       case $ip_first_octet[0]{
         10: {
-          $logserver_ip   = $profiles::logbroker_extranet::preprod_ip_address
-          $logserver_cert = $profiles::logbroker_extranet::preprod_logstash_forwarder_cert
+          $logserver_ip   = hiera('logbroker_extranet_preprod_ip_address')
+          $logserver_cert = hiera('logbroker_extranet_preprod_logstash_forwarder_cert')
         }
 
         192: {
-          $logserver_ip   = $profiles::log_repository::preprod_ip_address
-          $logserver_cert = $profiles::log_repository::preprod_logstash_forwarder_cert
+          $logserver_ip   = hiera('log_repository_preprod_ip_address')
+          $logserver_cert = hiera('log_repository_preprod_logstash_forwarder_cert')
         }
 
         default: {
@@ -66,8 +63,14 @@ class profiles::logstash_forwarder(
   }
 
   class { 'logstashforwarder':
-    servers => [ $logserver_ip ],
-    ssl_ca  => $logserver_cert,
-    require => File[logstash_forwarder_cert]
+    package_url => 'https://download.elastic.co/logstash-forwarder/binaries/logstash-forwarder-0.4.0-1.x86_64.rpm',
+    servers     => [ "${logserver_ip}:5000" ],
+    ssl_ca      => '/etc/pki/tls/certs/logstash-forwarder.crt',
+    require     => File['logstash_forwarder_cert']
+  }
+
+  logstashforwarder::file { 'stdlogs':
+    paths  => [ '/var/log/messages','/var/log/secure' ],
+    fields => { 'type' => 'syslog' },
   }
 }
