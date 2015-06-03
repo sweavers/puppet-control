ec1f83a0c105b468deda9ffa71a1f905637cec58
Andrew Moore <mooreandrewlr@gmail.com>
2015-06-03 14:03:34 +0100

    Securing Kibana

--- site/profiles/manifests/log_repository.pp
+++ site/profiles/manifests/log_repository.pp
@@ -5,6 +5,9 @@
 #
 class profiles::log_repository(
   $hostnumber     = 1,
+  $auth_basic     = 'Restricted',
+  $auth_password     = 'password',
+
 ){
 
   $logserver_cert = hiera('log_repository_logstash_forwarder_cert')
@@ -35,13 +38,29 @@ class profiles::log_repository(
     nodenumber  => $hostnumber
   }
 
-  class { 'nginx': }
+  include profiles::nginx
 
+  selinux::module { 'log_repo':
+    ensure => 'present',
+    source => 'puppet:///modules/profiles/log_repo.te'
+  }
+
+  # Set auth_basic to Off to disable.
   nginx::resource::vhost { 'kibana_proxy':
     server_name    => [ $::hostname ],
     listen_port    => 80,
+    auth_basic     => $auth_basic,
+    auth_basic_user_file => '/etc/nginx/conf.d/kibana.htpasswd',
     proxy_redirect => 'off',
     proxy          => 'http://127.0.0.1:5601'
+  }
+
+  file { '/etc/nginx/conf.d/kibana.htpasswd':
+  ensure => present,
+  }->
+  htpasswd { 'dan':
+    cryptpasswd => '{PLAIN}password',  # encrypted password
+    target      => '/etc/nginx/conf.d/kibana.htpasswd',
   }
 
   class { 'logstash':
