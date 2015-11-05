7e86002b7a0f6e0582f138ac537928d1550f94fa
Ben Fairless <ben.fairless@landregistry.gsi.gov.uk>
2015-11-05 16:34:05 +0000

    Fixed linting issues

--- site/ha/tests/localhost.pp
+++ site/ha/tests/localhost.pp
@@ -1,5 +1,5 @@
 class { ha:
-  backends => ['beta.registerview.landregistryconcept.co.uk:80'],
-  interface => 'eth0',
+  backends   => ['google.com'],
+  interface  => 'eth0',
   virtual_ip => '192.168.99.10'
 }
