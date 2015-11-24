a946e05a49d29af5a1f017452b850f3a13ae9b74
stevechambers <stevechambers@users.noreply.github.com>
2015-11-24 13:55:13 +0000

    Add high availabillity to rabbit profile

--- site/profiles/manifests/rabbitmq.pp
+++ site/profiles/manifests/rabbitmq.pp
@@ -19,8 +19,14 @@ class profiles::rabbitmq(
 
   $port              = 5672,
   $version           = '3.4.4',
-  $delete_guest_user = true
-  
+  $delete_guest_user = true,
+  $default_user      = 'guest',
+  $default_pass      = 'guest',
+  $cluster           = false,
+  $cluster_nodes     = [],
+  $erlang_cookie     = 'super_secret_key',
+  $admin_enable      = false
+
 ){
 
   # Load SELinuux policy for RabbitMQ
@@ -40,12 +46,36 @@ class profiles::rabbitmq(
 
   include ::erlang
 
-  class { '::rabbitmq':
-    version           => $ver,
-    port              => $port,
-    require           => Class[erlang],
-    delete_guest_user => $delete_guest_user
+  case $cluster {
+    true : {
+      class { '::rabbitmq':
+        version                  => $ver,
+        port                     => $port,
+        default_user             => $default_user,
+        default_pass             => $default_pass,
+        delete_guest_user        => $delete_guest_user,
+        cluster_nodes            => $cluster_nodes,
+        cluster_node_type        => 'disc',
+        erlang_cookie            => $erlang_cookie,
+        wipe_db_on_cookie_change => true,
+        config_cluster           => true,
+        admin_enable             => $admin_enable,
+        require                  => Class[erlang]
+      }
+    }
+    default : {
+      class { '::rabbitmq':
+        version           => $ver,
+        port              => $port,
+        default_user      => $default_user,
+        default_pass      => $default_pass,
+        delete_guest_user => $delete_guest_user,
+        admin_enable      => $admin_enable,
+        require           => Class[erlang]
+      }
+    }
   }
+
 
   include stdlib
 
