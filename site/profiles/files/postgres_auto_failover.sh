58f44972f9becaac6d60ea76649a3b7180c783d6
lezlightlr <lezlightlr@gmail.com>
2015-12-17 15:23:20 +0000

    modifications to autofailover script because of barman comments in postgresql.conf

--- site/profiles/files/postgres_auto_failover.sh
+++ site/profiles/files/postgres_auto_failover.sh
@@ -1,1 +1,3 @@
+sed -i -e 's/#BARMAN# //' -e '/archive_command = false/ d' /var/lib/pgsql/9.4/data/postgresql.conf
+/usr/pgsql-9.4/bin/pg_ctl reload
 /usr/pgsql-9.4/bin/repmgr -f /etc/repmgr/9.4/repmgr.conf --verbose standby promote
