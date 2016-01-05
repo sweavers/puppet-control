25710223524b2ed0da30665ddfb8f7a154d59476
Andrew Moore <mooreandrewlr@gmail.com>
2016-01-05 12:24:20 +0000

    Added Network Location Secrets

--- site/profiles/manifests/digitalregister_app.pp
+++ site/profiles/manifests/digitalregister_app.pp
@@ -67,7 +67,7 @@ class profiles::digitalregister_app(
   if $applications {
     $defaults = {
       'vc_app_host' => hiera('vc_app_host', 'http://localhost'),
-      'vc_app_token' =>  hiera('vc_app_token', 'http://localhost'),
+      'vc_app_token' =>  hiera('vc_app_token', ''),
     }
     create_resources('wsgi::application', $applications, $defaults)
   }
