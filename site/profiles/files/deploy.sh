a4395a803369ade8832a9408a458763d5f727eca
Andy Kerslake <AndyKerslake@users.noreply.github.com>
2016-06-07 15:40:28 +0100

    Remove compressed version of artifact.
    Code added to keep the /tmp folder filling up with un-necessary compressed artifacts.

--- site/profiles/files/deploy.sh
+++ site/profiles/files/deploy.sh
@@ -40,7 +40,7 @@ for PUPPET in ${PUPPETMASTER} ; do
   fi
 
   # SSH to puppet master, stop puppetmaster service, purge existing environments,
-  # uncompress artifact and restart puppetmaster service
+  # uncompress artifact and restart puppetmaster service then delete compressed artifact.
   ssh -o StrictHostKeyChecking=no deployment@${PUPPET} "sudo systemctl stop puppetmaster-unicorn.service" > /dev/null 2>&1
   [[ $? != '0' ]] && echo "Error stopping puppetmaster service on ${PUPPET}" | output ERROR && exit 1
   ssh deployment@${PUPPET} "sudo rm -rf /etc/puppet/environments" #> /dev/null 2>&1
@@ -53,6 +53,9 @@ for PUPPET in ${PUPPETMASTER} ; do
   else
     echo "Error restarting puppetmaster service on ${PUPPET}" | output ERROR && exit 1
   fi
+  echo "Removing compressed version of artifact."
+  rm /tmp/${ARTIFACT}
+  [[ $? != '0' ]] && echo "Error removing compressed version of artifact on  ${PUPPET}" | output ERROR && exit 1
 done
 
 # Copy artifact to remote ci server
