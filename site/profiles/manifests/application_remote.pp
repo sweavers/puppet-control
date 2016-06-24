e068cffeb30113e1f50b055503b5fe9adbca7bb2
Andrew Moore <mooreandrewlr@gmail.com>
2016-06-24 14:12:06 +0100

    Fixed top-scope variable issue

--- site/profiles/manifests/application_remote.pp
+++ site/profiles/manifests/application_remote.pp
@@ -8,7 +8,7 @@ class profiles::application_remote (
 
   if $ham_host and $ham_host_token {
 
-    $applications = remote_json("${ham_host}/api/host/${fqdn}", $ham_host_token)
+    $applications = remote_json("${ham_host}/api/host/${::fqdn}", $ham_host_token)
 
     if $applications {
       # Dirty hack to address hard coded logging location in manage.py
