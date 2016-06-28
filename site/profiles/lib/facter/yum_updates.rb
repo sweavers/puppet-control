ba7464a205fef529878c399182c0baa236922ca4
Andrew Moore <mooreandrewlr@gmail.com>
2016-06-28 20:44:18 +0100

    Yum Facts Refactored

--- site/profiles/lib/facter/yum_updates.rb
+++ site/profiles/lib/facter/yum_updates.rb
@@ -2,14 +2,18 @@
 # Additional Facts for when yum update was last run and amount of yum packages to update
 #
 
-Facter.add('yum_available_updates') do
-  setcode do
-    Facter::Core::Execution.exec('yum check-update --quiet | grep "^[a-z0-9]" | wc -l')
-  end
-end
+require 'fileutils'
 
-Facter.add('yum_last_update') do
-  setcode do
-    Facter::Core::Execution.exec('cat /var/log/yum.log | grep Updated: | tail -1 | awk \'{print $1 " " $2 " " $3}\'')
-  end
-end
+# Create facts directory if it doesn't exist
+FileUtils.mkdir_p '/etc/facter/facts.d/'
+
+# Create a new file called yum_updates.txt
+open('/etc/facter/facts.d/yum_updates.txt', 'w') { |f|
+  # Create a variable called "yum_available_updates" and write the amount of yum updates available to it
+  value = Facter::Core::Execution.exec('yum check-update --quiet | grep "^[a-z0-9]" | wc -l')
+  f.puts "yum_available_updates=" + value
+
+  # Create a variable called "yum_last_update" and write the last time an update was run
+  value = Facter::Core::Execution.exec('cat /var/log/yum.log | grep Updated: | tail -1 | awk \'{print $1 " " $2 " " $3}\'')
+  f.puts "yum_last_update=" + value
+}
