2e0f8e973c59a2170078d55d5f6ad7e1c55d0390
cs027sw <cs027sw@localhost.localdomain>
2016-07-19 15:17:50 +0100

    Updated gitlab module for new version of gitlab

--- site/profiles/manifests/gitlab.pp
+++ site/profiles/manifests/gitlab.pp
@@ -8,7 +8,7 @@
 #  ['local_login']     - Should local login should be permitted. (Boolean)
 #
 # Requires:
-# - spuder/gitlab
+# - vshn/gitlab
 # - puppetlabs/firewall
 #
 # Sample Usage:
@@ -16,29 +16,20 @@
 #
 class profiles::gitlab (
 
-  $enable_backup   = false,
-  $backup_location = undef,
-  $smtp_relay      = undef,
-  $local_login     = false,
   $external_url    = 'http://localhost',
-  $ldap_enabled    = false,
-  $ldap_host       = undef,
-  $ldap_base       = undef,
-  $ldap_bind_dn    = undef,
-  $ldap_password   = undef,
+  $enable_backup   = false,
+  $backup_location = 'undef',
   $gitlab_crt      = '',
   $gitlab_key      = '',
-  $https_redirect  = false,
-  $aws_acccess_key = undef,
-  $aws_secret_key  = undef
+  $ldap_config     = 'puppet:///site/profiles/files/ldap_conf.erb',
 
 ){
 
-
-  $gitlab_download_link = $::operatingsystem ? {
-    CentOS     => 'https://downloads-packages.s3.amazonaws.com/centos-7.0.1406/gitlab-7.5.1_omnibus.5.2.0.ci-1.el7.x86_64.rpm',
-    Redhat     => 'https://downloads-packages.s3.amazonaws.com/centos-7.0.1406/gitlab-7.5.1_omnibus.5.2.0.ci-1.el7.x86_64.rpm',
-    Ubuntu     => 'https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.5.1-omnibus.5.2.0.ci-1_amd64.deb',
+  file { '/etc/gitlab' :
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0700'
   }
 
   file { '/etc/gitlab/ssl' :
@@ -65,36 +56,31 @@ class profiles::gitlab (
   }
 
   class { '::gitlab' :
-    puppet_manage_config          => true,
-    puppet_manage_backups         => $enable_backup,
-    gitlab_branch                 => '7.5.1',
-    gitlab_download_link          => $gitlab_download_link,  # Should be pulled from Hiera
-    external_url                  => $external_url,
-    backup_keep_time              => 5184000, # In seconds, 5184000 = 60 days
-    backup_path                   => '/var/opt/gitlab/backups',
-    gravatar_enabled              => true,
-    gitlab_signup_enabled         => false,
-    gitlab_signin_enabled         => $local_login,
-    gitlab_default_theme          => 4, # 1=Basic, 2=Mars, 3=Modern, 4=Gray, 5=Color (gitlab default: 2)
-    gitlab_default_projects_limit => 40,
+    external_url        => $external_url,
+    require             => File['/etc/gitlab/ssl/gitlab.crt', '/etc/gitlab/ssl/gitlab.key'],
 
-    # LDAP integration
-    ldap_enabled                  => $ldap_enabled,
-    ldap_host                     => $ldap_host,
-    ldap_port                     => 389,
-    ldap_method                   => 'plain',
-    ldap_base                     => $ldap_base,
-    ldap_bind_dn                  => $ldap_bind_dn,
-    ldap_password                 => $ldap_password,
-    ldap_uid                      => 'sAMAccountName',
+    gitlab_rails        => {
+      gitlab_default_theme  => 4,
+      #ldap_enabled          => false,
+      #ldap_servers          => $ldap_config,
+      backup_path           => '/var/opt/gitlab/backups',
+      backup_keep_time      => '5184000', # In seconds, 5184000 = 60 days
+    },
 
-    #ssl configuration
-    ssl_certificate               => '/etc/gitlab/ssl/gitlab.crt',
-    ssl_certificate_key           => '/etc/gitlab/ssl/gitlab.key',
-    redirect_http_to_https        => true,
-    require                       => File['/etc/gitlab/ssl/gitlab.crt', '/etc/gitlab/ssl/gitlab.key']
+    nginx               => {
+      redirect_http_to_https => true,
+      ssl_certificate        => '/etc/gitlab/ssl/gitlab.crt',
+      ssl_certificate_key    => '/etc/gitlab/ssl/gitlab.key',
+    },
 
+    logging             => {
+      svlogd_size     => 209715200,
+      svlogd_num      => 30,
+      svlogd_timeout  => 86400,
+      svlogd_filter   => 'gzip',
+    },
   }
+
 
   #Set up s3 backups if on AWS
   if $::hosting_platform == AWS {
@@ -110,8 +96,6 @@ class profiles::gitlab (
     }
   }
 
-  #ensure that nfs package is installed
-  #determin the package name based on the OS
   $nfs_package = $::operatingsystem ? {
     CentOS     => 'nfs-utils',
     Redhat     => 'nfs-utils',
@@ -135,9 +119,4 @@ class profiles::gitlab (
     }
   }
 
-  file_line { 'internal smtp relay config':
-    ensure => present,
-    line   => "relayhost = ${smtp_relay}",
-    path   => '/etc/postfix/main.cf',
-  }
 }
