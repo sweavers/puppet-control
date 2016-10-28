f270cfca4810723fd80c63e4e42d7578d99ff0ae
Chris Hatton <christopher.hatton@digital.landregistry.gov.uk>
2016-10-28 11:04:48 +0100

    correct lint issues

--- site/profiles/manifests/docker.pp
+++ site/profiles/manifests/docker.pp
@@ -1,35 +1,46 @@
+# Class profiles::docker
+# This class will manage docker installations from their repo
+#
+# Parameters:
+#  NIL
+#
+# Sample Usage:
+#   class { 'profiles::docker': }
+#
+#
+
 class profiles::docker (
 
 
 ){
 
 yumrepo { 'DockerRepository':
-  ensure    => 'present',
-  baseurl   => 'https://yum.dockerproject.org/repo/main/centos/$releasever/',
-  enabled   => 1,
-  gpgcheck  => 1,
-  gpgkey    => 'https://yum.dockerproject.org/gpg'
+  ensure   => 'present',
+  baseurl  => 'https://yum.dockerproject.org/repo/main/centos/$releasever/',
+  enabled  => 1,
+  gpgcheck => 1,
+  gpgkey   => 'https://yum.dockerproject.org/gpg'
 }
 
 package {'python-pip' :
-    ensure => present,
+  ensure => present,
 }
 
 package { 'docker-engine' :
-    ensure => present,
-    require => Yumrepo['DockerRepository']
+  ensure  => present,
+  require => Yumrepo['DockerRepository']
   }
 
 package {'backports.ssl-match-hostname' :
-     provider => pip,
-     ensure   => '3.5.0.1',
-     require  => Package['python-pip']
+  ensure   => '3.5.0.1',
+  provider => pip,
+  require  => Package['python-pip']
 }
 
 package {'docker-compose' :
-   provider => pip,
-   ensure   => present,
-   require  => Package['backports.ssl-match-hostname']
+  ensure   => present,
+  provider => pip,
+  require  => Package['backports.ssl-match-hostname']
 }
 
 }
