53400710df2867de4680f7fdfc037ee6335a0c31
steve <steve.chambers@digital.landregistry.gov.uk>
2017-01-13 12:37:11 +0000

    support module refactor

--- site/profiles/manifests/nagios_client.pp
+++ site/profiles/manifests/nagios_client.pp
@@ -1,33 +1,124 @@
 class profiles::nagios_client(
 
-  $interface = 'eth1'
+  $interface       = eth1,
+  $nagios_services = hiera_hash('nagios_services', false)
 
   ){
 
-  $nagiosclient_ip = inline_template("<%= scope.lookupvar('::ipaddress_${interface}') -%>")
-
-  include nagiosclient
+  # create additional nrpe commands from hiera
+  class { 'nagiosclient':
+    nrpe_commands => hiera_array('nrpe_commands', undef)
+  }
 
   # Export nagios host configuration
+  @@nagios_host { $::fqdn :
+    ensure                => present,
+    alias                 => $::hostname,
+    address               => inline_template("<%= scope.lookupvar('::ipaddress_${interface}') -%>"),
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'linux-server',
+    max_check_attempts    => '5',
+    check_period          => '24x7',
+    notification_interval => '30',
+    notification_period   => '24x7'
+  }
 
-  #if $nagios::params::nagios_server_ip != $nagiosclient_ip {
-    @@nagios_host { $::hostname :
-      ensure                => present,
-      alias                 => $::hostname,
-      display_name          => $::hostname,
-      address               => $nagiosclient_ip,
-      use                   => 'generic-server,server-graph',
-      hostgroups            => 'linux-servers, linux-virtual-servers',
-      contact_groups        => 'admins',
-      mode                  => '0644',
-      owner                 => 'root',
-      max_check_attempts    => '5',
-      check_period          => '24x7',
-      notification_interval => '30',
-      notification_period   => '24x7',
-      icon_image_alt        => 'Linux',
-      statusmap_image       => 'vendor-logos/linux.jpg',
-      icon_image            => 'vendor-logos/linux.jpg'
+  # Export nagios service configuration
+  @@nagios_service { "check_ping_${::hostname}":
+    ensure              => present,
+    check_command       => 'check_ping!100.0,20%!500.0,60%',
+    mode                => '0644',
+    owner               => root,
+    use                 => 'generic-service',
+    host_name           => $::hostname,
+    notification_period => '24x7',
+    service_description => 'Ping'
+  }
+
+  @@nagios_service { "check_load_${::hostname}":
+    ensure              => present,
+    check_command       => 'check_nrpe!check_load\!5.0,4.0,3.0\!10.0,6.0,4.0',
+    mode                => '0644',
+    owner               => root,
+    use                 => 'generic-service',
+    host_name           => $::hostname,
+    notification_period => '24x7',
+    service_description => 'Current Load'
+  }
+
+  @@nagios_service { "check_current_users_${::hostname}":
+    ensure              => present,
+    check_command       => 'check_nrpe!check_users\!20\!50',
+    mode                => '0644',
+    owner               => root,
+    use                 => 'generic-service',
+    host_name           => $::hostname,
+    notification_period => '24x7',
+    service_description => 'Current Users'
+  }
+
+  @@nagios_service { "check_root_partition_${::hostname}":
+    ensure              => present,
+    check_command       => 'check_nrpe!check_disk\!20%\!10%\!/',
+    mode                => '0644',
+    owner               => root,
+    use                 => 'generic-service',
+    host_name           => $::hostname,
+    notification_period => '24x7',
+    service_description => 'Root Partition'
+  }
+
+  @@nagios_service { "check_ssh_${::hostname}":
+    ensure              => present,
+    check_command       => 'check_ssh',
+    mode                => '0644',
+    owner               => root,
+    use                 => 'generic-service',
+    host_name           => $::hostname,
+    notification_period => '24x7',
+    service_description => 'SSH'
+  }
+
+  # Only add swap check if this is not an aws machine
+  if ($::hosting_platform != dev_aws) and ($::hosting_platform != internal_aws) {
+    @@nagios_service { "check_swap_${::hostname}":
+      ensure              => present,
+      check_command       => 'check_nrpe!check_swap\!20\!10',
+      mode                => '0644',
+      owner               => root,
+      use                 => 'generic-service',
+      host_name           => $::hostname,
+      notification_period => '24x7',
+      service_description => 'Swap Usage'
     }
-  #}
+  }
+
+  @@nagios_service { "check_procs_${::hostname}":
+    ensure              => present,
+    check_command       => 'check_nrpe!check_procs\!250\!400\!RSZDT',
+    mode                => '0644',
+    owner               => root,
+    use                 => 'generic-service',
+    host_name           => $::hostname,
+    notification_period => '24x7',
+    service_description => 'Processes'
+  }
+
+  @@nagios_service { "check_mem_${::hostname}":
+    ensure              => present,
+    check_command       => 'check_nrpe!check_mem\!20\!10',
+    mode                => '0644',
+    owner               => root,
+    use                 => 'generic-service',
+    host_name           => $::hostname,
+    notification_period => '24x7',
+    service_description => 'Ram Usage'
+  }
+
+  # create additional nagios services from hiera
+  if $nagios_services {
+    create_resources('@@nagios_service', $nagios_services)
+  }
+
 }
--- site/profiles/manifests/nagios_server.pp
+++ site/profiles/manifests/nagios_server.pp
@@ -1,10 +1,78 @@
-class profiles::nagios_server(){
+class profiles::nagios_server(
 
-  include nagios
+  $nagios_hosts         = hiera_hash('nagios_hosts',false),
+  $nagios_hostgroups    = hiera_hash('nagios_hostgroups',false),
+  $nagios_services      = hiera_hash('nagios_services',false),
+  $nagios_commands      = hiera_hash('nagios_commands',false),
+  $nagios_contacts      = hiera_hash('nagios_contacts',false),
+  $nagios_contactgroups = hiera_hash('nagios_contactgroups',false)
 
-  # Auto populate nagios configuration from puppetdb
+  ){
+
+  include nagiosserver
+
+  # Remove monitoring for localhost
+  file { '/etc/nagios/objects/localhost.cfg' :
+    ensure  => absent,
+    require => Package['nagios']
+  }
+
+  # Collect nagios resources from puppetdb
   Nagios_host <<||>> {
    require => Package['nagios'],
    notify  => Service['nagios']
+  }
+
+  Nagios_hostgroup <<||>> {
+   require => Package['nagios'],
+   notify  => Service['nagios']
+  }
+
+  Nagios_service <<||>> {
+    require => Package['nagios'],
+    notify  => Service['nagios']
+  }
+
+  Nagios_command <<||>> {
+    require => Package['nagios'],
+    notify  => Service['nagios']
+  }
+
+  # Import nagios resources from heira
+
+  $resource_defaults = {notify  => Service['nagios']}
+
+  if $nagios_hosts {
+    create_resources('nagios_host', $nagios_hosts, $resource_defaults)
+  }
+
+  if $nagios_hostgroups {
+    create_resources('nagios_hostgroup', $nagios_hostgroups, $resource_defaults)
+  }
+
+  if $nagios_services {
+    create_resources('nagios_service', $nagios_services, $resource_defaults)
+  }
+
+  if $nagios_commands {
+    create_resources('nagios_command', $nagios_commands, $resource_defaults)
+  }
+
+  if $nagios_contacts {
+    create_resources('nagios_contact', $nagios_contacts, $resource_defaults)
+  }
+
+  if $nagios_contactgroups {
+    create_resources('nagios_contactgroup', $nagios_contactgroups, $resource_defaults)
+  }
+
+  # Configure nagios not to monitor localhosts
+  file_line { 'nagios_localhost':
+    ensure  => present,
+    path    => '/etc/nagios/nagios.cfg',
+    line    => '#cfg_file=/etc/nagios/objects/localhost.cfg',
+    match   => 'cfg_file=/etc/nagios/objects/localhost.cfg',
+    notify  => Service['nagios'],
+    require => Package['nagios']
   }
 }
