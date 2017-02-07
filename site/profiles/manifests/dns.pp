f224745d23df0bb9f903a22da515a3370019a7c5
Ben Fairless <ben.fairless@landregistry.gov.uk>
2017-02-07 10:55:37 +0000

    Added DNS management profile

---
+++ site/profiles/manifests/dns.pp
@@ -0,0 +1,56 @@
+# Class profiles::dns
+#
+# This class will manage DNS records for servers and supports multiple interfaces
+#
+# Requires:
+# - puppetlabs/stdlib
+# - landregistry/powerdns
+#
+# Sample Usage:
+#   class { 'profiles::dns':
+#     interfaces => {
+#       eth0 => {
+#         prepend => '',
+#         append  => '-mgmt',
+#         zone    => 'host.dev.ctp.local'
+#       },
+#       eth1 => {
+#         prepend => '',
+#         append  => '',
+#         zone    => 'host.dev.ctp.local'
+#       }
+#     }
+#   }
+#
+class profiles::dns (
+
+  $interfaces = hiera_hash('interfaces',undef)
+
+){
+
+  include ::stdlib
+
+  if has_key($interfaces, 'eth0') {
+    $eth0 = $interfaces['eth0']
+    $eth0_hostname = "${eth0['prepend']}${hostname}${eth0['append']}"
+    powerdns::record { $eth0_hostname :
+      ensure   => present,
+      type     => 'A',
+      ttl      => '3600',
+      zone     => $eth0['zone'],
+      content  => [ $::ipaddress_eth0 ]
+    }
+  }
+
+  if has_key($interfaces, 'eth1') {
+    $eth1_hostname = "${interfaces['eth1']['prepend']}${hostname}${interfaces['eth1']['append']}"
+    powerdns::record { $eth1_hostname :
+      ensure   => present,
+      type     => 'A',
+      ttl      => '3600',
+      zone     => $interfaces['eth1']['zone'],
+      content  => [ $::ipaddress_eth1 ]
+    }
+  }
+
+}
