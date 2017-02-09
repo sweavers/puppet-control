2ad89b010228871ceacc3e7ec32a94b73d011e5e
Ben Fairless <ben.fairless@landregistry.gov.uk>
2017-02-09 15:18:53 +0000

    Added check for dns interfaces hash

--- site/profiles/manifests/dns.pp
+++ site/profiles/manifests/dns.pp
@@ -30,27 +30,29 @@ class profiles::dns (
 
   include ::stdlib
 
-  if ($::ipaddress_eth0 and has_key($interfaces, 'eth0') and has_key($interfaces['eth0'], 'zone')) {
-    $eth0 = $interfaces['eth0']
-    $eth0_hostname = "${eth0['prepend']}${::hostname}${eth0['append']}"
-    powerdns::record { $eth0_hostname :
-      ensure  => present,
-      type    => 'A',
-      ttl     => '3600',
-      zone    => $eth0['zone'],
-      content => [ $::ipaddress_eth0 ]
+  if is_hash($interfaces) {
+    if ($::ipaddress_eth0 and has_key($interfaces, 'eth0') and has_key($interfaces['eth0'], 'zone')) {
+      $eth0 = $interfaces['eth0']
+      $eth0_hostname = "${eth0['prepend']}${::hostname}${eth0['append']}"
+      powerdns::record { $eth0_hostname :
+        ensure  => present,
+        type    => 'A',
+        ttl     => '3600',
+        zone    => $eth0['zone'],
+        content => [ $::ipaddress_eth0 ]
+      }
     }
-  }
 
-  if ($::ipaddress_eth1 and has_key($interfaces, 'eth1') and has_key($interfaces['eth1'], 'zone')) {
-    $eth1 = $interfaces['eth1']
-    $eth1_hostname = "${eth1['prepend']}${::hostname}${eth1['append']}"
-    powerdns::record { $eth1_hostname :
-      ensure  => present,
-      type    => 'A',
-      ttl     => '3600',
-      zone    => $eth1 ['zone'],
-      content => [ $::ipaddress_eth1 ]
+    if ($::ipaddress_eth1 and has_key($interfaces, 'eth1') and has_key($interfaces['eth1'], 'zone')) {
+      $eth1 = $interfaces['eth1']
+      $eth1_hostname = "${eth1['prepend']}${::hostname}${eth1['append']}"
+      powerdns::record { $eth1_hostname :
+        ensure  => present,
+        type    => 'A',
+        ttl     => '3600',
+        zone    => $eth1 ['zone'],
+        content => [ $::ipaddress_eth1 ]
+      }
     }
   }
 
