1706c52e8cd510493595809a29541dafe1352b66
steve <steve.chambers@digital.landregistry.gov.uk>
2017-02-20 14:52:29 +0000

    Check and notification improvements

--- site/profiles/manifests/nagios_client.pp
+++ site/profiles/manifests/nagios_client.pp
@@ -42,6 +42,13 @@ class profiles::nagios_client(
 
   ){
 
+  # Set notification_period to managed hours if aws_dev machine
+  if ($::hosting_platform == dev_aws) {
+    $time_period = 'dev_aws'
+  } else {
+    $time_period = '24x7'
+  }
+
   # create additional nrpe commands from hiera
   class { 'nagiosclient':
     nrpe_commands => hiera_array('nrpe_commands', undef)
@@ -56,110 +63,126 @@ class profiles::nagios_client(
     owner                 => root,
     use                   => 'linux-server',
     max_check_attempts    => '5',
-    check_period          => '24x7',
-    notification_interval => '30',
-    notification_period   => '24x7',
+    check_period          => $time_period,
+    notification_interval => '0',
+    notification_period   => $time_period,
     contact_groups        => 'admins'
   }
 
   # Export nagios service configuration
   @@nagios_service { "check_ping_${::hostname}":
-    ensure              => present,
-    check_command       => 'check_ping!100.0,20%!500.0,60%',
-    mode                => '0644',
-    owner               => root,
-    use                 => 'generic-service',
-    host_name           => $::hostname,
-    notification_period => '24x7',
-    service_description => 'Ping',
-    contact_groups      => 'admins'
+    ensure                => present,
+    check_command         => 'check_ping!100.0,20%!500.0,60%',
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'generic-service',
+    host_name             => $::hostname,
+    check_period          => $time_period,
+    notification_period   => $time_period,
+    notification_interval => '0',
+    service_description   => 'Ping',
+    contact_groups        => 'admins'
   }
 
   @@nagios_service { "check_load_${::hostname}":
-    ensure              => present,
-    check_command       => 'check_nrpe!check_load\!5.0,4.0,3.0\!10.0,6.0,4.0',
-    mode                => '0644',
-    owner               => root,
-    use                 => 'generic-service',
-    host_name           => $::hostname,
-    notification_period => '24x7',
-    service_description => 'Current Load',
-    contact_groups      => 'admins'
+    ensure                => present,
+    check_command         => 'check_nrpe!check_load\!5.0,4.0,3.0\!10.0,6.0,4.0',
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'generic-service',
+    host_name             => $::hostname,
+    check_period          => $time_period,
+    notification_period   => $time_period,
+    notification_interval => '0',
+    service_description   => 'Current Load',
+    contact_groups        => 'admins'
   }
 
   @@nagios_service { "check_current_users_${::hostname}":
-    ensure              => present,
-    check_command       => 'check_nrpe!check_users\!20\!50',
-    mode                => '0644',
-    owner               => root,
-    use                 => 'generic-service',
-    host_name           => $::hostname,
-    notification_period => '24x7',
-    service_description => 'Current Users',
-    contact_groups      => 'admins'
+    ensure                => present,
+    check_command         => 'check_nrpe!check_users\!20\!50',
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'generic-service',
+    host_name             => $::hostname,
+    check_period          => $time_period,
+    notification_period   => $time_period,
+    notification_interval => '0',
+    service_description   => 'Current Users',
+    contact_groups        => 'admins'
   }
 
   @@nagios_service { "check_root_partition_${::hostname}":
-    ensure              => present,
-    check_command       => 'check_nrpe!check_disk\!20%\!10%\!/',
-    mode                => '0644',
-    owner               => root,
-    use                 => 'generic-service',
-    host_name           => $::hostname,
-    notification_period => '24x7',
-    service_description => 'Root Partition',
-    contact_groups      => 'admins'
+    ensure                => present,
+    check_command         => 'check_nrpe!check_disk\!20%\!10%\!/',
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'generic-service',
+    host_name             => $::hostname,
+    check_period          => $time_period,
+    notification_period   => $time_period,
+    notification_interval => '0',
+    service_description   => 'Root Partition',
+    contact_groups        => 'admins'
   }
 
   @@nagios_service { "check_ssh_${::hostname}":
-    ensure              => present,
-    check_command       => 'check_ssh',
-    mode                => '0644',
-    owner               => root,
-    use                 => 'generic-service',
-    host_name           => $::hostname,
-    notification_period => '24x7',
-    service_description => 'SSH',
-    contact_groups      => 'admins'
+    ensure                => present,
+    check_command         => 'check_ssh',
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'generic-service',
+    host_name             => $::hostname,
+    check_period          => $time_period,
+    notification_period   => $time_period,
+    notification_interval => '0',
+    service_description   => 'SSH',
+    contact_groups        => 'admins'
   }
 
   # Only add swap check if this is not an aws machine
   if ($::hosting_platform != dev_aws) and ($::hosting_platform != internal_aws) {
     @@nagios_service { "check_swap_${::hostname}":
-      ensure              => present,
-      check_command       => 'check_nrpe!check_swap\!20\!10',
-      mode                => '0644',
-      owner               => root,
-      use                 => 'generic-service',
-      host_name           => $::hostname,
-      notification_period => '24x7',
-      service_description => 'Swap Usage',
-      contact_groups      => 'admins'
+      ensure                => present,
+      check_command         => 'check_nrpe!check_swap\!20\!10',
+      mode                  => '0644',
+      owner                 => root,
+      use                   => 'generic-service',
+      host_name             => $::hostname,
+      check_period          => $time_period,
+      notification_period   => $time_period,
+      notification_interval => '0',
+      service_description   => 'Swap Usage',
+      contact_groups        => 'admins'
     }
   }
 
   @@nagios_service { "check_procs_${::hostname}":
-    ensure              => present,
-    check_command       => 'check_nrpe!check_procs\!250\!400\!RSZDT',
-    mode                => '0644',
-    owner               => root,
-    use                 => 'generic-service',
-    host_name           => $::hostname,
-    notification_period => '24x7',
-    service_description => 'Processes',
-    contact_groups      => 'admins'
+    ensure                => present,
+    check_command         => 'check_nrpe!check_procs\!250\!400\!RSZDT',
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'generic-service',
+    host_name             => $::hostname,
+    check_period          => $time_period,
+    notification_period   => $time_period,
+    notification_interval => '0',
+    service_description   => 'Processes',
+    contact_groups        => 'admins'
   }
 
   @@nagios_service { "check_mem_${::hostname}":
-    ensure              => present,
-    check_command       => 'check_nrpe!check_mem\!20\!10',
-    mode                => '0644',
-    owner               => root,
-    use                 => 'generic-service',
-    host_name           => $::hostname,
-    notification_period => '24x7',
-    service_description => 'Ram Usage',
-    contact_groups      => 'admins'
+    ensure                => present,
+    check_command         => 'check_nrpe!check_mem\!20\!10',
+    mode                  => '0644',
+    owner                 => root,
+    use                   => 'generic-service',
+    host_name             => $::hostname,
+    check_period          => $time_period,
+    notification_period   => $time_period,
+    notification_interval => '0',
+    service_description   => 'Ram Usage',
+    contact_groups        => 'admins'
   }
 
   # create additional nagios services from hiera
