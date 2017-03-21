83b411f2181ff98afbc9bc9b7113be5bbed3f805
steve <steve.chambers@digital.landregistry.gov.uk>
2017-03-21 11:59:46 +0000

    Disable http checks add tcp checks

--- site/profiles/manifests/application.pp
+++ site/profiles/manifests/application.pp
@@ -25,22 +25,43 @@ class profiles::application (
     }
   }
 
-  # Define http_check for bespoke applications
-  define http_check($bind){
-    @@nagios_service { "${::hostname}-lr-${name}-http_check" :
-      ensure                => present,
-      check_command         => "check_nrpe!check_service_http\!'127.0.0.1'\!'${bind}'\!'200 OK'",
-      mode                  => '0644',
-      owner                 => root,
-      use                   => 'generic-service',
-      host_name             => $::hostname,
-      check_period          => '24x7',
-      contact_groups        => 'admins',
-      notification_interval => 0,
-      notification_period   => '24x7',
-      service_description   => "LR http ${name}"
+  # Define http_check for bespoke applications - not currently in use
+  # define http_check($bind){
+  #   @@nagios_service { "${::hostname}-lr-${name}-http_check" :
+  #     ensure                => present,
+  #     check_command         => "check_nrpe!check_service_http\!'127.0.0.1'\!'${bind}'\!'200 OK'",
+  #     mode                  => '0644',
+  #     owner                 => root,
+  #     use                   => 'generic-service',
+  #     host_name             => $::hostname,
+  #     check_period          => '24x7',
+  #     contact_groups        => 'admins',
+  #     notification_interval => 0,
+  #     notification_period   => '24x7',
+  #     service_description   => "LR http ${name}"
+  #   }
+  # }
+
+  # Define tcp_check for bespoke applications
+  define tcp_check($bind){
+    unless $bind==0 or $bind=='0' or $bind=='' or $bind== undef {
+      @@nagios_service { "${::hostname}-lr-${name}-tcp_check" :
+        ensure                => present,
+        check_command         => "check_nrpe!check_service_tcp\!'127.0.0.1'\!'${bind}'",
+        mode                  => '0644',
+        owner                 => root,
+        use                   => 'generic-service',
+        host_name             => $::hostname,
+        check_period          => '24x7',
+        contact_groups        => 'admins',
+        notification_interval => 0,
+        notification_period   => '24x7',
+        service_description   => "LR tcp ${name}"
+      }
     }
   }
+
+
 
   if $applications {
     # Dirty hack to address hard coded logging location in manage.py
@@ -65,6 +86,10 @@ class profiles::application (
     service_check{$services:}
 
     # Create http check for each application specified for server
-    create_resources(http_check, $bindhash)
+    # create_resources(http_check, $bindhash)
+
+    # Create tcp check for each application specified for server
+    create_resources(tcp_check, $bindhash)
+
   }
 }
