072506476d0edfa7b1d69440be926ffe9f5dd718
Ben Fairless <ben.fairless@landregistry.gov.uk>
2017-04-06 11:04:44 +0100

    Update powerdns.pp

--- site/profiles/manifests/powerdns.pp
+++ site/profiles/manifests/powerdns.pp
@@ -14,6 +14,14 @@ class profiles::powerdns (
 
   include ::stdlib
 
-  ensure_resource('class', 'powerdns::server::authoritive', $config)
+  class { powerdns::server::authoritive :
+    ensure     => present,
+    port       => 53,
+    password   => ${config['password'],
+    api_port   => ${config['api_port'],
+    soa_name   => ${config['soa_name'],
+    soa_mail   => ${config['soa_mail'],
+    postgresql => ${config['postgresql']
+  }
 
 }
